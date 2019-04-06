2db87e7a65bb1ff25de104d60fc6c300cc9091a6
magallegos1996 <bwcekos@gmail.com>
2019-04-06 00:24:11 -0500

    Se prueba mi primera ruta en express con exito

--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,42 @@
+const path = require('path');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
+const multer = require('multer');
+const express = require('express');
+const routes = require('../routes/index.js');
+
 module.exports = app => {
     //settings
-    app.set('port', 3000);
-    return app;
+    app.set('port', 3002);
 
-}
+    app.set('views', path.join(__dirname, 'views'));
+    app.engine('.hbs', exphbs({
+        defaultLayout: 'main' ,
+        partialsDir: path.join(app.get('views'), 'partials'),
+        layoutsDir: path.join(app.get('views'), 'layouts'),
+        extname: '.hbs',
+        //son funciones que pueden ser usadas en handlebars, no es lo mismo que los helpers de express
+        //helpers: require('./helpers'),
+    }));
+    app.set('view engine', '.hbs');
+
+    //middlewares
+    app.use(morgan('dev'));
+    app.use(multer({
+        //cuando se suba una imagen se crearrá en la ruta /src/public/upload/temp
+        dest: path.join(__dirname, '../public/upload/temp')
+    }).single('image'));
+    //sirve para obtener los datos que vienen de formularios
+    app.use(express.urlencoded({
+        extended: false
+    }));
+    app.use(express.json());
+
+    //routes
+    routes(app);
+
+
+    //errorhandlers
+
+    return app;
+};
